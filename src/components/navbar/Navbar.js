143a6e2f19623ade3f4d45726d1fb2dc3708de45
mukesh <yogahindu1999@gmail.com>
2021-10-10 11:57:27 +0530

    changes added

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -14,24 +14,38 @@ import {
     Link
 } from "react-router-dom";
 const Navbar = () => {
+    // var rightPart = document.querySelector('.rightPart');
+
+    // var MaxWidth991 = window.matchMedia("(max-width: 991px)");
+
+    // function match() {
+    //     // MaxWidth991.matches ? classList.add('col-10') : classList.remove('full-page');
+    //     if (MaxWidth991.matches) {
+    //         rightPart.classList.remove('justify-content-end')
+    //     }
+
+    // }
+
+    // MaxWidth991.addEventListener("resize", match);
+    // match();
     return (
         <div className="nav">
             {/* nav class */}
-            <div className="container-md p-4" >
-                <div className="row" id="row" >
+            <div className="p-4 container-md" >
+                <div className="row" id="row">
 
-                    <div className="col-md-12 col-lg-3  leftPart">
-                        <div className="row">
-                            <div className="col-6">
+                    <div className="col-md-12 col-lg-3 d-flex leftPart">
+                        <div className="row flex-nowrap">
+                            <div className="col-6" id="col-6">
                                 <input type="text" className="search" placeholder="Search.." />
                             </div>
-                            <div className="col-6">
+                            <div className="col-6" id="col-2">
                                 <AiOutlineSearch />
                             </div>
                         </div>
                     </div>
 
-                    <div className="col-md-12 col-lg-6  middlePart">
+                    <div className="col-md-12 col-lg-6 middlePart">
                         {/* <div className="nav__middlepart-linksdiv"> */}
                         <ul className="row middlePart__row">
                             <li className="col-2 list-unstyled"><Link className="text-decoration-none link" to="/home" exact>Home</Link></li>
@@ -44,13 +58,22 @@ const Navbar = () => {
                         {/* </div> */}
                     </div>
 
-                    <div className="col-md-12 col-lg-3  d-flex justify-content-end rightPart">
-                        <ul className="row">
-                            <li className="col-3 list-unstyled"><a className="text-decoration-none" href="https://www.facebook.com" ><GrFacebookOption /></a></li>
-                            <li className="col-3 list-unstyled"><a className="text-decoration-none" href="https://www.instagram.com/" ><AiOutlineInstagram /></a></li>
-                            <li className="col-3 list-unstyled"><a className="text-decoration-none" href="https://www.pinterest.com/" ><FaPinterestP /></a></li>
-                            <li className="col-3 list-unstyled"><a className="text-decoration-none" href="https://www.twitter.com" ><IoLogoTwitter /></a></li>
-                        </ul>
+                    <div className="col-md-12 col-lg-3 d-flex justify-content-end rightPart">
+                        <div className="row">
+                            <div className="col-md-10 start">
+                                <ul className="row flex-nowrap">
+                                    <li className="col-3 list-unstyled"><a className="text-decoration-none" href="https://www.facebook.com" ><GrFacebookOption /></a></li>
+                                    <li className="col-3 list-unstyled"><a className="text-decoration-none" href="https://www.instagram.com/" ><AiOutlineInstagram /></a></li>
+                                    <li className="col-3 list-unstyled"><a className="text-decoration-none" href="https://www.pinterest.com/" ><FaPinterestP /></a></li>
+                                    <li className="col-3 list-unstyled"><a className="text-decoration-none" href="https://www.twitter.com" ><IoLogoTwitter /></a></li>
+                                </ul>
+                            </div>
+
+                            <div className="col-md-2 show-ham">
+                                <GiHamburgerMenu />
+                            </div>
+                        </div>
+
                     </div>
 
                 </div>
--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Navbar from '../../components/navbar/Navbar'
 import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import home from "../../public/image/tour1.webp";
+import tour2 from "../../public/image/tour2.webp";
+import tour3 from "../../public/image/tour3.webp";
+import tour4 from "../../public/image/tour4.webp";
 import "./Home.css"
 const Home = () => {
     return (
-        <div>
+        <div className="container-fulid">
             <div>
-                <Navbar />
+                <Navbar style={{ position: "fixed" }} />
             </div>
             <div className="homeImg">n
                 <div className=" text">
@@ -16,7 +18,33 @@ const Home = () => {
                     <p>I haven’t been everywhere, but it’s on my list </p>
                 </div>
             </div>
+
+            <div className="py-4 container-md middleSection">
+                <div className="row">
+                    <div className="text-center">
+                        <p>
+                            I'm a paragraph. Click here to add your own text and edit me. It’s easy. <br />
+                            Just click “Edit Text” or double click me to add your own content and make changes to the font.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="container-md ">
+                <div className="row travel-img">
+                    <div className="col-4 ml-5 image1">
+                        <a href="/travel">Travel</a>
+                    </div>
+                    <div className="col-4 image2">
+                        <a href="/travel">Eat</a>
+                    </div>
+                    <div className="col-4 image3" >
+                        <a href="/travel">Relax</a>
+                    </div>
+                </div>
+            </div>
         </div>
+
     )
 }
 
